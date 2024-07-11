fbd5ea2e65322ef0d8947288e8a0a55f164f17e3
maksym solomyanov <maksym.solomyanov@gmail.com>
2024-07-11 10:56:28 +0200

    Add QR code to generated PDF ticket.

--- ticket.py
+++ ticket.py
@@ -6,7 +6,7 @@ from qrcode import QRCode
 
 class Ticket:
     def __init__(self, user: str, price, seat: str):
-        generate_id = ''.join(random.choices(string.ascii_letters, k=6))
+        generate_id = ''.join(random.choices(string.ascii_letters, k=10))
         self.ticket_id = generate_id
         self.user = user
         self.price = price
@@ -25,29 +25,34 @@ class Ticket:
         pdf.set_font("Arial", "B", 15)
         pdf.cell(w=40, h=10, txt="Name", border=1)
         pdf.set_font("Arial", "", 13)
-        pdf.cell(w=0, h=10, txt=self.user, border=1, ln=1)
+        pdf.cell(w=100, h=10, txt=self.user, border=1, ln=1)
+        pdf.image(f'{self.seat}_qr.png', w=40, h=40, x=155, y=41)
 
         pdf.set_font("Arial", "B", 15)
         pdf.cell(w=40, h=10, txt="Ticket ID", border=1)
         pdf.set_font("Arial", "", 13)
-        pdf.cell(w=0, h=10, txt=self.ticket_id, border=1, ln=1)
+        pdf.cell(w=100, h=10, txt=self.ticket_id, border=1, ln=1)
 
         pdf.set_font("Arial", "B", 15)
         pdf.cell(w=40, h=10, txt="Price", border=1)
         pdf.set_font("Arial", "", 13)
-        pdf.cell(w=0, h=10, txt=f"{self.price}", border=1, ln=1)
+        pdf.cell(w=100, h=10, txt=f"{self.price}", border=1, ln=1)
 
         pdf.set_font("Arial", "B", 15)
         pdf.cell(w=40, h=10, txt="Seat Number", border=1)
         pdf.set_font("Arial", "", 13)
-        pdf.cell(w=0, h=10, txt=self.seat, border=1, ln=1)
-        #pdf.image(f'{self.seat}_qr.png')
+        pdf.cell(w=100, h=10, txt=self.seat, border=1, ln=1)
 
         pdf.output(f"{self.seat}_ticket.pdf")
 
     def generate_qr(self):
         qr_code = QRCode(box_size=10, border=1)
-        qr_code.add_data(f'Ticket ID: {self.ticket_id}, Seat: {self.seat}')
+        qr_code.add_data(f'Ticket ID: {self.ticket_id}, Name: {self.user}, '
+                         f'Seat: {self.seat}, Price: {self.price}')
         qr_image = qr_code.make_image(fill_color='black', back_color='white')
         qr_image.save(f'{self.seat}_qr.png')
 
+
+if __name__ == '__main__':
+    ticket = Ticket('John Smith', 50, 'A99')
+    ticket.to_pdf()
